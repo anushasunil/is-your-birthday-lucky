12dc93ca568c98eb8afe956aa5517bb61837c59a
Anusha <anushasunil71201@gmail.com>
2021-09-14 04:54:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector(".btn-check");
+const messageOutput = document.querySelector(".message-box")
