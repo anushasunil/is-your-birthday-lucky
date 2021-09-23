9da448aa74ea93f1f8c2c1126d300f0f2ab65aa6
Anusha <anushasunil71201@gmail.com>
2021-09-23 09:42:47 +0530

    refactor: code format

--- index.js
+++ index.js
@@ -4,72 +4,54 @@ const checkButton = document.querySelector(".btn-check");
 const messageOutput = document.querySelector(".message-box")
 
 
-    checkButton.addEventListener("click", function checkIfYourBirthdateIsLucky(){
-        if( dateOfBirth.value && luckyNumber.value)
-        {
-            console.log(dateOfBirth.value, luckyNumber.value);
-            const dob = dateOfBirth.value;
-            const sum = calculateSum(dob); 
-            let result = compareValues(sum, luckyNumber);
-            printMessage(result);
-        }
-        else
-        {
-            messageOutput.innerHTML = "please enter a value 🙄";
-        }
-        
-    
-    });
-
-
-function printMessage(result)
-{
-    messageOutput.style.fontSize = "1.5rem";
-    if(result)
-    {
-        messageOutput.innerHTML = "Yayy! " + luckyNumber.value + " is your lucky number! 😃"
+checkButton.addEventListener("click", function checkIfYourBirthdateIsLucky() {
+    if (dateOfBirth.value && luckyNumber.value) {
+        console.log(dateOfBirth.value, luckyNumber.value);
+        const dob = dateOfBirth.value;
+        const sum = calculateSum(dob);
+        let result = compareValues(sum, luckyNumber);
+        printMessage(result);
+    } else {
+        messageOutput.innerHTML = "please enter a value 🙄";
     }
-    else
-    {
+
+
+});
+
+
+function printMessage(result) {
+    messageOutput.style.fontSize = "1.5rem";
+    if (result) {
+        messageOutput.innerHTML = "Yayy! " + luckyNumber.value + " is your lucky number! 😃"
+    } else {
         messageOutput.innerHTML = "uh oh! " + luckyNumber.value + " is not your lucky number! 😶"
     }
 }
 
-function compareValues(sum, luckyNumber)
-{
-    if(sum % Number(luckyNumber.value) === 0)
-    {
+function compareValues(sum, luckyNumber) {
+    if (sum % Number(luckyNumber.value) === 0) {
         return true;
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
 
-function calculateSum(dob)
-{
+function calculateSum(dob) {
     dobFiltered = replaceAll(dob, "-", "");
     let sum = 0;
-    for(let index = 0; index < dobFiltered.length; index = index + 1)
-    {
+    for (let index = 0; index < dobFiltered.length; index = index + 1) {
         sum = sum + Number(dobFiltered.charAt(index));
     }
     console.log(sum)
     return sum
 }
 
-function replaceAll(str, beforeValue, afterValue)
-{
+function replaceAll(str, beforeValue, afterValue) {
     let finalString = ""
-    for(let i = 0; i < str.length; i = i + 1 )
-    {
-        if(str[i] == beforeValue)
-        {
+    for (let i = 0; i < str.length; i = i + 1) {
+        if (str[i] == beforeValue) {
             finalString = finalString + afterValue;
-        }
-        else
-        {
+        } else {
             finalString = finalString + str[i];
         }
     }
