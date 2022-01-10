25769414390f8c33b9709f71e4be1cf9dda7d9f2
Anusha <anushasunil71201@gmail.com>
2022-01-10 12:23:58 +0530

    refactor: modified function declaration format

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const checkButton = document.querySelector(".btn-check");
 const messageOutput = document.querySelector(".message-box")
 
 
-checkButton.addEventListener("click", function checkIfYourBirthdateIsLucky() {
+checkButton.addEventListener("click", () => {
     if (dateOfBirth.value && luckyNumber.value) {
         console.log(dateOfBirth.value, luckyNumber.value);
         const dob = dateOfBirth.value;
@@ -15,46 +15,24 @@ checkButton.addEventListener("click", function checkIfYourBirthdateIsLucky() {
         messageOutput.innerHTML = "please enter a value 🙄";
     }
 
-
 });
 
 
-function printMessage(result) {
-    messageOutput.style.fontSize = "1.5rem";
-    if (result) {
-        messageOutput.innerHTML = "Yayy! " + luckyNumber.value + " is your lucky number! 😃"
-    } else {
-        messageOutput.innerHTML = "uh oh! " + luckyNumber.value + " is not your lucky number! 😶"
-    }
+const printMessage = (result) => {
+    result ? messageOutput.innerHTML = `Yayy! ${luckyNumber.value} is your lucky number! 😃` : messageOutput.innerHTML = `uh oh! ${luckyNumber.value} is not your lucky number! 😶`
 }
 
-function compareValues(sum, luckyNumber) {
-    if (sum % Number(luckyNumber.value) === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+const compareValues = (sum, luckyNumber) => (sum % Number(luckyNumber.value) === 0) ? true : false;
 
-function calculateSum(dob) {
+const calculateSum = (dob) => {
     dobFiltered = replaceAll(dob, "-", "");
     let sum = 0;
-    for (let index = 0; index < dobFiltered.length; index = index + 1) {
-        sum = sum + Number(dobFiltered.charAt(index));
-    }
-    console.log(sum)
+    for (let index = 0; index < dobFiltered.length; index++) sum = sum + Number(dobFiltered.charAt(index));
     return sum
 }
 
-function replaceAll(str, beforeValue, afterValue) {
+const replaceAll = (str, beforeValue, afterValue) => {
     let finalString = ""
-    for (let i = 0; i < str.length; i = i + 1) {
-        if (str[i] == beforeValue) {
-            finalString = finalString + afterValue;
-        } else {
-            finalString = finalString + str[i];
-        }
-    }
-    console.log(finalString)
+    for (let i = 0; i < str.length; i = i + 1)(str[i] == beforeValue) ? finalString += afterValue : finalString += str[i];
     return finalString
 }
